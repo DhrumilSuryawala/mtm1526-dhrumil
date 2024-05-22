5eb44b2bdcf3744ce776551e9a227e03d5f1c1ff
Dhrumil Suryawala <162204609+DhrumilSuryawala@users.noreply.github.com>
2024-05-21 22:21:32 -0400

    Add files via upload

---
+++ JS/index.js
@@ -0,0 +1,47 @@
+//Task1
+
+let a = 3;
+let b = 5;
+let c;
+
+let result =`
+a + b = ${a + b}\n 
+
+a - b = ${a - b}\n
+
+a * b = ${a * b}\n
+
+a % b = ${a % b}\n
+
+a / b = ${a / b}\n
+
+a -= b = ${a -= b}\n
+
+a *= b = ${a *= b}\n 
+
+a /= b = ${a /= b }\n
+
+a %= b = ${a %= b}\n
+
+a == b = ${a == b}\n
+
+a != b = ${a != b}\n
+
+a > b = ${a > b}\n
+
+a < b = ${a < b}\n
+
+!a && !c = ${!a && !c}\n
+
+!a || !c = ${!a || !c}\n 
+`
+console.log(result);
+
+//Task2
+
+let fname = 'Dhrumil';
+let lname = 'Suryawala';
+let email = 'Sur0005@algonquinlive.com';
+
+let output = `My name is ${fname} ${lname}. And you can contact me at ${email}`;
+console.log (output);
